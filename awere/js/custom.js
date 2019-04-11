6146dace170919a142125d4c5ac7f624c09eafda
janjoeflamingo <49531701+janjoeflamingo@users.noreply.github.com>
2019-04-11 21:46:16 +0300

    Add files via upload

---
+++ awere/js/custom.js
@@ -0,0 +1,48 @@
+$(function(){
+	$('.tabs a').click(function(){
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false
+	});
+	$('.accordeon__head').on('click', function(){
+		var el = $(this);
+		el.next('.accordeon__body').slideToggle();
+		el.toggleClass('open');
+		return false;
+	});
+
+	$('.styler').styler();
+	// $('.toggle-filter').click(function(){
+	// 	$(this).toggleClass('active');
+	// 	$('.sidebar').toggleClass('open');
+	// 	$('.box-product').toggleClass('full');
+	// })
+
+	// $('.toggle-menu').click(function(){
+	// 	$(this).toggleClass('active');
+	// 	$('.dropdown-content--menu').toggleClass('open');
+	// })
+
+	// $('.styler').styler();
+
+	$( "#slider-range" ).slider({
+		range: true,
+		min: 0,
+		max: 500,
+		values: [ 75, 300 ],
+		slide: function( event, ui ) {
+			$( "#amount" ).val( "$" + ui.values[ 0 ] );
+			$( "#amount1" ).val( "$" + ui.values[ 1 ] );
+		}
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );
+    $( "#amount1" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
+	
+	// $('.dropdown-content__title').click(function(){
+	// 	$(this).toggleClass('d-open');
+	// })
+
+});
