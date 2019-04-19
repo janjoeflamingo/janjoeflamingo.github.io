07ceb9a9b4847882a037574f48bfe883f671dddd
janjoeflamingo <van.loner@mail.ru>
2019-04-19 18:23:09 +0400

    slider

--- awere/js/custom.js
+++ awere/js/custom.js
@@ -46,3 +46,57 @@ $(function(){
 	// })
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onload = function() {
+
+// 	var images = document.querySelectorAll('#slider .images img');
+// 	var i = 0;
+
+// 	document.getElementById('btn-prev').onclick = function(){
+// 		images[i].className = '';
+// 		i--;
+
+// 		if(i < 0){
+// 			i = images.length - 1;
+// 		}
+
+// 		images[i].className = 'active';
+// 	}
+// 	document.getElementById('btn-next').onclick = function(){
+// 		images[i].className = '';
+// 		i++;
+
+// 		if(i >= images.length){
+// 			i=0;
+// 		}
+// 		images[i].className = 'active';
+// 	}
+// }
